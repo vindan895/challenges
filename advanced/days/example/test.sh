c04038dcb1ff0f3227a1e8cd039d38d559e69795
5late <christianrfernandes5@gmail.com>
2022-10-20 00:09:16 -0400

    moved test file to specific day

---
+++ advanced/days/example/test.sh
@@ -0,0 +1,10 @@
+for solution in solutions/*/*.py; do
+	python3 $solution > $solution.output
+
+	if [[ -n $(diff $solution.output ./answer.txt) ]]; then
+		echo FAIL - $solution
+	else
+		echo SUCCESS - $solution
+	fi
+done
+
