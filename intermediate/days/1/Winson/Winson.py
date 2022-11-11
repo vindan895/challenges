2ff6be7d41fa5ac35f06a461d33430039f0ed529
winsonC10 <116374820+winsonC10@users.noreply.github.com>
2022-11-11 12:25:51 -0500

    Intermediate Day 1
    My Intermediate Day 1 code

---
+++ intermediate/days/1/Winson/Winson.py
@@ -0,0 +1,23 @@
+# Strings
+p1 = "fads5464gfgfdg4fd5s6474h4gffk44fdh4sfv14fn4m487iodr754hdf2g1"
+p2 = "hj87mkff4s5d64787uytk4gh41n3dsgapnl4kjh78yu412gkfhj4f578iyt"
+p3 = "rqups45sdj7m43xby7874kghg4g7iupl4zcv4fad448784asfdf"
+
+# Length of p1
+i = 0
+for s in p1:
+  i = i+1
+print(i)
+
+#Length of p2
+i = 0
+for s in p2:
+  i = i+1
+print(i)
+
+#Length of p3
+i = 0
+for s in p3:
+  i = i+1
+print(i)
+
